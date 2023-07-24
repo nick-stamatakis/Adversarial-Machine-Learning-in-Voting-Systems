540c96aaa753d8acd14d8f00af1dfbf9c46fea8a
Nicholas Nicos Stamatakis <116750704+nick-stamatakis@users.noreply.github.com>
2023-07-24 15:02:37 -0400

    Update ExtractBubblesFromWhitespacePNG.py

--- ExtractBubblesFromWhitespacePNG.py
+++ ExtractBubblesFromWhitespacePNG.py
@@ -2,10 +2,14 @@ import os
 from PIL import Image
 
 # Open the output sheet image
-output_sheet_path = "AlignedImages/AlignedDenseNet_Adv_P2.png"
+output_sheet_path = "AlignedImagesSimpleCNN_Val/AlignedSimpleCNN_Val_P11.png"
+i = 960 #0,96,192,288,384,480, 576, 672, 768, 864, 960
 output_sheet = Image.open(output_sheet_path)
 
+Labels_array = [0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0]
+
 # Constants for bubble layout
+BATCH_SIZE = 64
 PAGE_WIDTH = int(8.5 * 200)  # 8.5 inches converted to pixels at 200 dpi
 PAGE_HEIGHT = int(11 * 200)  # 11 inches converted to pixels at 200 dpi
 MARGIN = int(1 * 200)  # 1 inch margin converted to pixels at 200 dpi
@@ -14,12 +18,12 @@ BUBBLE_HEIGHT = 40  # Bubble height in pixels
 SPACING = int(0.5 * 200)  # 0.5 inch spacing converted to pixels at 200 dpi
 
 # Calculate the number of bubbles that can fit on the sheet
-num_columns = int((PAGE_WIDTH - 2 * MARGIN + SPACING) / (BUBBLE_WIDTH + SPACING))
-num_rows = int((PAGE_HEIGHT - 2 * MARGIN + SPACING) / (BUBBLE_HEIGHT + SPACING))
+num_columns = int((PAGE_WIDTH - 2 * MARGIN) / (BUBBLE_WIDTH + SPACING))
+num_rows = int((PAGE_HEIGHT - 2 * MARGIN) / (BUBBLE_HEIGHT + SPACING))
 total_bubbles = num_columns * num_rows
 
 # Directory to save the extracted bubbles
-extracted_bubbles_directory = "DenseNet_Adv_ExtractedBubbles"
+extracted_bubbles_directory = "SimpleCNN_Val_EXTRACTED"
 
 # Create the directory if it doesn't exist
 os.makedirs(extracted_bubbles_directory, exist_ok=True)
@@ -37,15 +41,19 @@ for row in range(num_rows):
         bubble = output_sheet.crop(bubble_box)
 
         # Save the extracted bubble
-        bubble_index = existing_bubbles + row * num_columns + col + 1
-        bubble_filename = f"bubble_{bubble_index}.png"
+        Label = Labels_array[i]
+        batch_index = (existing_bubbles + row * num_columns + col) // BATCH_SIZE
+        example_index = (existing_bubbles + row * num_columns + col) % BATCH_SIZE
+        bubble_filename = f"{batch_index}th Batch {example_index}th Example_{Label}.png"
         bubble_path = os.path.join(extracted_bubbles_directory, bubble_filename)
         bubble.save(bubble_path)
 
         x += BUBBLE_WIDTH + SPACING
+        print(i)
+        i+=1
 
     # Move to the next row
     x = MARGIN
     y += BUBBLE_HEIGHT + SPACING
 
-print("Individual bubbles have been extracted and saved in the 'ExtractedBubbles' directory.")
+print("Individual bubbles have been extracted and saved in the 'SimpleCNN_Val_EXTRACTED' directory.")
